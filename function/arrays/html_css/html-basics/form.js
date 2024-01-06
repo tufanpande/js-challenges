d702bc13ed8fa75db7367b79299ead3d8f6f9ae8
Tufan <tufanpande@gmail.com>
2024-01-06 20:25:55 +0545

    Html/css-JS

---
+++ function/arrays/html_css/html-basics/form.js
@@ -0,0 +1,18 @@
+
+
+function today(){
+    return new Date();
+}
+ 
+function getFormData(){
+    const fName = document.getElementById("fName").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const male = document.getElementById("male").value;
+    const female = document.getElementById("female").value;
+    const message = document.getElementById("message").value;
+    const subscribe = document.getElementById("subscribe").value;
+    console.log({fName, email, password,male,female, message, subscribe});
+}
+
+document.getElementById("datId").innerHTML=today();
